8c27252c37d6590eadafd30b93acebf5d5e90bcb
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2023-09-04 17:14:55 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <Button>button</Button>
+    </div>
+  );
+}
+
+export default App;
